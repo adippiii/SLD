39081f04cc710ab0b953a85e040b77e3ca775d0a
InfinityOdds <aadip2311@gmail.com>
2025-02-16 10:36:56 -0600

    meow

--- sign_detection.py
+++ sign_detection.py
@@ -25,7 +25,10 @@ def detect_fingers():
     labels_dict = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z', 26: ' '}
 
     letters = []
-    word = [];
+    word = []
+    punctuation = []
+    sentence = []
+
     keyboard = Controller()
 
     while True:
@@ -91,8 +94,18 @@ def detect_fingers():
                     engine.say(res)
                     engine.runAndWait()
                     print(res)
+                    word = []
 
-                    word = []    
+                    sentence.append(res)
+
+                    punctuation.append(' ')
+                    if len(punctuation) == 2:
+                        res = ' '.join(sentence)
+                        engine.say(res)
+                        engine.runAndWait()
+                        print(res)
+                        sentence = []
+                        punctuation = []
                 letters = []
 
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
